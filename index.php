a9b23a8fc6f13f781239e81a7171ee6fc5167549
chilledguru <chilledguru@gmail.com>
2019-07-30 16:38:56 +0500

    Update index.php
    API has been updated in the script

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ There are no limits on filesize and downloads, so you can download unlimited fil
         if(ytId !== false) {
           
           $iframe = $('<iframe>');
-          $iframe.attr({src:'https://yt-mp3s.com/mp3/'+ytId, frameBorder: 0});
+          $iframe.attr({src:'https://yt-mp3s.com/@api/button/mp3/'+ytId, frameBorder: 0});
           $iframe.css({width:'100%', height:'100px'});
           
           //Remove iframe if one exists (on second url some will exists)
